911992653e54301c2cb77fb9b8e7798ded8580c8
Ivonne <ivonne.rodriguez@treage.net>
2025-05-19 03:46:24 +0200

    Update files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
-import FirstPart from './components/FirstPart.jsx';
-import MainStructure from './components/MainStructure.jsx';
-import SecondPart from './components/SecondPart.jsx';
-import ThirdPart from './components/ThirdPart.jsx';
-import FourthPart from './components/FourthPart.jsx';
-import FifthPart from './components/FifthPart.jsx';
-import SixthPart from './components/SixthPart.jsx';
-import SeventhPart from './components/SeventhPart.jsx';
+import FirstPart from './components/FirstPart';
+import MainStructure from './components/MainStructure';
+import SecondPart from './components/SecondPart';
+import ThirdPart from './components/ThirdPart';
+import FourthPart from './components/FourthPart';
+import FifthPart from './components/FifthPart';
+import SixthPart from './components/SixthPart';
+import SeventhPart from './components/SeventhPart';
 // Import Bootstrap CSS and JS
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 function App() {
   return (
     <div className="App">
