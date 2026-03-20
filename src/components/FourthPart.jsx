cc1aaf8b75a386b958e0b8546bb6d3cc49898817
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-03-20 23:19:46 +0100

    Add new room: Active Reconnaissance

--- src/components/FourthPart.jsx
+++ src/components/FourthPart.jsx
@@ -35,6 +35,7 @@ import bgd34 from '../img/badges/bgd34.png';
 import bgd35 from '../img/badges/bgd35.png';
 import bgd36 from '../img/badges/bgd36.png';
 import bgd37 from '../img/badges/bgd37.png';
+import bgd38 from '../img/badges/bgd38.png';
 
 function FourthPart() {
   const badges = [
@@ -291,7 +292,13 @@ function FourthPart() {
       benefits: '.',
       image: bgd37,
     },
-
+ {
+      title: 'Active Reconnaissance Room ',
+      description:
+      '.',
+      benefits: '.',
+      image: bgd38,
+    },
   ];
   return (
     <div>
