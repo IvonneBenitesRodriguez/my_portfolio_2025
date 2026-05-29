8d6d06ef6908513ac3055ceefb26b42a9bbee718
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-05-29 21:39:18 +0200

    Add content for Command Injection Room

--- src/components/FourthPart.jsx
+++ src/components/FourthPart.jsx
@@ -297,9 +297,8 @@ function FourthPart() {
     },
       {
       title: 'Command Injection Room ',
-      description:
-      '.',
-      benefits: '.',
+      description:'This room focuses on running arbitrary commands on a web server through practical exercises to practice exploiting command injection vulnerabilities.',
+      benefits: 'Understand Operating System command injection attack vectors and learn defensive techniques to prevent RCE (Remote Code Execution).',
       image: bgd37,
     },
       {
